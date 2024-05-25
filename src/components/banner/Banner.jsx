be840aebca05a99f85dd6eed7850e67eee4da899
ionic shofiq <ionicshofic@gmail.com>
2024-05-25 00:16:06 -0700

    added hot deal part

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,10 +4,11 @@ import banner from '../../assets/banner.jpg';
 const Banner = () => {
 	return (
 		<div className='flex'>
-			<div className='bg-[#576BB2] h-80 lg:w-48 lg:block hidden mt-3'></div>
+			<div className='bg-[#576BB2] h-80 lg:w-32 lg:block hidden mt-3'></div>
 			<div className='flex flex-col-reverse md:flex-row mt-3'>
-				<div className='bg-white mx-auto h-80 md:w-60  pt-2 border-b-2'>
-					<ul className='w-56 flex flex-col justify-between items-center'>
+				<div className='bg-white mx-auto h-80 w-60 pt-2 border-b-2'>
+					
+					<ul className=''>
 						<li className='py-[1px] flex justify-between items-center px-2'>
 							<a href='#'>Mens Shopping</a>
 							<FaChevronRight className='opacity-60 text-xs' />
@@ -20,19 +21,19 @@ const Banner = () => {
 							<a href='#'>Household</a>
 							<FaChevronRight className='opacity-60 text-xs' />
 						</li>
-						<li className='py-[1px] flex justify-between items-center md:px-2'>
+						<li className='py-[1px] flex justify-between items-center px-2'>
 							<a href='#'>Kitchen And Dining</a>
 							<FaChevronRight className='opacity-60 text-xs' />
 						</li>
-						<li className='py-[1px] flex justify-between items-center md:px-2'>
+						<li className='py-[1px] flex justify-between items-center px-2'>
 							<a href='#'>Computer Accessories</a>
 							<FaChevronRight className='opacity-60 text-xs' />
 						</li>
-						<li className='py-[1px] flex justify-between items-center md:px-2'>
+						<li className='py-[1px] flex justify-between items-center px-2'>
 							<a href='#'>Gadgets</a>
 							<FaChevronRight className='opacity-60 text-xs' />
 						</li>
-						<li className='py-[1px] flex justify-between items-center md:px-2'>
+						<li className='py-[1px] flex justify-between items-center px-2'>
 							<a href='#'>Jewellery</a>
 							<FaChevronRight className='opacity-60 text-xs' />
 						</li>
@@ -40,16 +41,17 @@ const Banner = () => {
 							<a href='#'>Home Decor</a>
 							<FaChevronRight className='opacity-60 text-xs' />
 						</li>
-						<li className='py-[1px] flex justify-between items-center md:px-2'>
+						<li className='py-[1px] flex justify-between items-center px-2'>
 							<a href='#'>Cosmetics</a>
 							<FaChevronRight className='opacity-60 text-xs' />
 						</li>
-						<li className='py-[1px] flex justify-between items-center md:px-2'>
+						<li className='py-[1px] flex justify-between items-center px-2'>
 							<a href='#'>All Categories &gt;&gt;</a>
 						</li>
+						
 					</ul>
 				</div>
-				<img className='md:h-80 md:w-auto' src={banner} alt='' />
+				<img className='md:h-80 lg:w-[900px]' src={banner} alt='' />
 			</div>
 			<div className='bg-[#576BB2] h-80 w-16 lg:block hidden mt-3'></div>
 		</div>
