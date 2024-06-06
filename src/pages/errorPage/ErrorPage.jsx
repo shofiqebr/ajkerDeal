4314e725ca794f42a42da418bcc7e14ea79c0650
shofiq <shofiqebr@gmail.com>
2024-06-06 16:28:44 +0600

    error helmet

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -1,10 +1,21 @@
 import errorImg from '../../assets/errorImg.jpg'
 import backImg from '../../assets/back.avif'
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const ErrorPage = () => {
     return (
         <div style={{backgroundImage: `url(${errorImg})`}} className='relative bg-no-repeat lg:bg-cover bg-center lg:bg-right-top  h-[80vh] lg:m-20 m-5 text-white lg:text-6xl text-xl font-bold text-center xl:pt-44 lg:pt-32 pt-44'>
+
+
+
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Ajker Deal | Error</title>
+                <link rel="canonical" href="http://static.ajkerdeal.com/images/dealdetails/ad-logo.svg" />
+            </Helmet>
+
+
             You are searching something <br /> unavailable 
             <Link to='/'>
             <div className=' absolute right-10 bottom-20 xl:bottom-5 flex flex-col justify-center items-center'>
